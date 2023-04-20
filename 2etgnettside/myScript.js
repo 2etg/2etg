3d4633f0dbafcbb8c1abacf3e971090ea640e582
2etg <131355917+2etg@users.noreply.github.com>
2023-04-20 12:12:36 +0200

    Add files via upload

---
+++ 2etgnettside/myScript.js
@@ -0,0 +1,11 @@
+function endreTekst(){
+ }
+
+function leggTilBilde(){
+document.getElementById("bildeboks").innerHTML += "<img src= \"bilder/edpV2.jfif\">";
+}
+ 
+function fjerneBilder(){
+    document.getElementById("bildeboks").innerHTML =""; 
+    document.getElementById("demo").innerHTML = "";
+    }
